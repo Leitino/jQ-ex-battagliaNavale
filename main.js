8fd3a2c7747ee8559b32533dddd25c3762d2fab0
Leitino <alessandro.leita89@gmail.com>
2018-04-04 18:23:22 +0200

    finito

--- main.js
+++ main.js
@@ -2,10 +2,31 @@ $(document).ready(function() {
 
   var navi = [];
   for (var i = 0; i < 15; i++) {
-  var navi_casuali = (Math.floor(Math.random()*64)+1)
+  var navi_casuali = (Math.floor(Math.random()*63)+1)
   navi.push(navi_casuali)
   }
-var rossi=0
-var verdi=0
+var con_navi_colpite=0
+var con_navi_mancate=0
 var nave = $('.nave')
-console.log(nave)
+
+nave.click(function() {
+  var una_nave = $(this).attr('id')
+  if (navi.includes(parseInt(una_nave))) {
+    $(this).addClass('red')
+    con_navi_colpite++
+    $('#navi_colpite').html(con_navi_colpite)
+  }
+  else {
+    $(this).addClass('green')
+    con_navi_mancate++
+    $('#navi_mancate').html(con_navi_mancate)
+  }
+  if (con_navi_colpite==15) {
+    alert('hai vinto')
+  }
+  else if (con_navi_mancate==50) {
+    alert('hai perso')    
+  }
+
+})
+})
